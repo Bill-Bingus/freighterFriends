6a9cfc676f076042032d21fcf30787fd91a31ec3
Nguyen-Alex <ducgau.nguyen@gmail.com>
2024-02-03 16:30:00 -0500

    added pfps onto map as icons

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,4 +1,8 @@
 import React, { useEffect } from 'react';
+import marker2 from "../assets/marker2.png";
+import marker3 from "../assets/marker3.png";
+import marker4 from "../assets/marker4.png";
+import marker1 from "../assets/marker1.png";
 
 const SimpleMarker = () => {
   useEffect(() => {
@@ -6,6 +10,8 @@ const SimpleMarker = () => {
       // Define the callback function that's called once the Google Maps script loads
       window.initMap = () => {
         const center = { lat: 40.44330978393555, lng: -79.94284057617188 };
+
+
         //const directionsService = new window.google.maps.DirectionsService();
         const directionsRenderer = new window.google.maps.DirectionsRenderer();
         const map = new window.google.maps.Map(document.getElementById('map'), {
@@ -34,6 +40,36 @@ const SimpleMarker = () => {
         requestDirections('new orleans, la', 'buffalo, ny');
         requestDirections('charleston', 'erie');
         requestDirections('kansas city, ks', 'new york, ny');
+
+        const markerTwo = new google.maps.Marker({
+          position: center,
+          icon: marker2,
+          map: map, 
+          size: new google.maps.Size(20, 20),
+        }); 
+
+        const markerThree = new google.maps.Marker({
+          position: {lat: 40.261836, lng: -76.898279},
+          icon: marker3,
+          map:map,
+          size: new google.maps.Size(20, 20),
+        });
+
+        const markerFour = new google.maps.Marker({
+          position: {lat: 39.942710, lng: -82.991934},
+          icon: marker4,
+          map:map,
+          size: new google.maps.Size(20, 20),
+        });
+
+        const markerOne = new google.maps.Marker({
+          position: {lat: 42.867451, lng: -78.804178},
+          icon: marker1,
+          map:map,
+          size: new google.maps.Size(20, 20),
+        });
+
+        
 
         /*const requestA = {origin: "pittsburgh", destination: "chicago", travelMode: "DRIVING"};
         directionsService.route(requestA, function (result, status) {
