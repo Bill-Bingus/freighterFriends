323e0343bc55310cfbb49dbd566c888ff3cf7b51
Nguyen-Alex <ducgau.nguyen@gmail.com>
2024-02-03 13:34:56 -0500

    updated functionality to include ability to draw multiple routes onto a single map

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -5,7 +5,7 @@ const SimpleMarker = () => {
     const loadGoogleMapsScript = () => {
       // Define the callback function that's called once the Google Maps script loads
       window.initMap = () => {
-        const center = { lat: 40.44330978393555, lng: -79.94284057617188 };
+        //const center = { lat: 40.44330978393555, lng: -79.94284057617188 };
         const directionsService = new window.google.maps.DirectionsService();
         const directionsRenderer = new window.google.maps.DirectionsRenderer();
         const map = new window.google.maps.Map(document.getElementById('map'), {
@@ -32,14 +32,14 @@ const SimpleMarker = () => {
 
     loadGoogleMapsScript();
     // Remove script and global initMap function when the component unmounts
-    /*return () => {
+    return () => {
       window.initMap = null;
       const scriptTag = document.querySelector('script[src^="https://maps.googleapis.com/maps/api/js"]');
       document.body.appendChild(scriptTag);
       if (scriptTag) {
         document.body.removeChild(scriptTag);
       }
-    };*/
+    };
   }, []); 
 
   return (
