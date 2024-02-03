facfcd97ce1a9a0c9627db9ead1133a1d8a07a62
Nguyen-Alex <ducgau.nguyen@gmail.com>
2024-02-03 14:09:28 -0500

    multi route functionality

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -5,22 +5,44 @@ const SimpleMarker = () => {
     const loadGoogleMapsScript = () => {
       // Define the callback function that's called once the Google Maps script loads
       window.initMap = () => {
-        //const center = { lat: 40.44330978393555, lng: -79.94284057617188 };
-        const directionsService = new window.google.maps.DirectionsService();
+        const center = { lat: 40.44330978393555, lng: -79.94284057617188 };
+        //const directionsService = new window.google.maps.DirectionsService();
         const directionsRenderer = new window.google.maps.DirectionsRenderer();
         const map = new window.google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
+          zoom: 6,
           center: center,
           mapId: 'Route Map'
         });
 
-        const request = {origin: "pittsburgh", destination: "chicago", travelMode: "DRIVING"};
-        directionsService.route(request, function (result, status) {
+        function renderDirections(result) {
+            var directionsRenderer = new google.maps.DirectionsRenderer;
+            directionsRenderer.setMap(map);
+            directionsRenderer.setDirections(result);
+        }
+            
+        const directionsService = new google.maps.DirectionsService;
+        function requestDirections(start, end) {
+            directionsService.route({
+                origin: start,
+                destination: end,
+                travelMode: google.maps.DirectionsTravelMode.DRIVING
+                }, function(result) {
+                    renderDirections(result);
+                });
+        }
+        requestDirections('pittsburgh, pa', 'chicago, il');
+        requestDirections('new orleans, la', 'buffalo, ny');
+        requestDirections('charleston', 'erie');
+        requestDirections('kansas city, ks', 'new york, ny');
+
+        /*const requestA = {origin: "pittsburgh", destination: "chicago", travelMode: "DRIVING"};
+        directionsService.route(requestA, function (result, status) {
             //if (status == "OK") {
                 directionsRenderer.setDirections(result);
             //}
         });
-        directionsRenderer.setMap(map)
+        //directionsRenderer.setMap(map)
+        directionsRenderer.setMap(map) */
       };
 
       // Load the Google Maps API script
